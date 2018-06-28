daf9b7474487b9f57e2b53ba9274bf5c5df941cd
SBats <simon.bats12@gmail.com>
2018-06-28 19:57:14 -0400

    Create door prefab and controller

---
+++ a-wrench-in-the-gears/Assets/Entities/World/Doors/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorController : MonoBehaviour {
+
+	public bool open = false;
+	public LeverController lever;
+
+	private UnityAction<bool> statusUpdateActions;
+	private Animator animator;
+
+	private void Awake() {
+		this.animator = GetComponent<Animator>();
+	}
+
+	private void Start() {
+		this.UpdateStatus(this.open);
+		if (this.lever) {
+			this.statusUpdateActions += this.UpdateStatus;
+			this.lever.subscribeToStateChange(UpdateStatus);
+		}
+	}
+
+	public void UpdateStatus(bool status) {
+		this.open = status;
+		this.animator.SetBool("open", status);
+	}
+}
