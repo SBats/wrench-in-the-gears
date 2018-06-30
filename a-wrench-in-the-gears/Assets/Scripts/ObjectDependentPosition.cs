9d1caab96780adb9851b0ce4f88275efdedb8b23
SBats <simon.bats12@gmail.com>
2018-06-30 11:15:09 -0400

    Fix clouds parallax suttering

--- a-wrench-in-the-gears/Assets/Scripts/ObjectDependentPosition.cs
+++ a-wrench-in-the-gears/Assets/Scripts/ObjectDependentPosition.cs
@@ -3,7 +3,8 @@
 public class ObjectDependentPosition : MonoBehaviour {
 
 	public GameObject refObject;
-	public float smoothingTime;
+	public bool reverseX;
+	public bool reverseY;
 	public bool horizontalDependent;
 	public bool verticalDependent;
 	public float horizontalOffset;
@@ -11,30 +12,34 @@ public class ObjectDependentPosition : MonoBehaviour {
 	public float progressiveHorizontalOffset;
 	public float progressiveVerticalOffset;
 
-	private Vector3 velocity = Vector3.zero;
-
 	private void Start () {
-		this.updatePosition(0);
+		this.updatePosition(false);
 	}
 
-	private void Update () {
-		this.updatePosition(this.smoothingTime);
+	private void LateUpdate () {
+		this.updatePosition();
 	}
 
-	private void updatePosition (float smoothing) {
+	private void updatePosition (bool smooth = true) {
 		if (!this.refObject) return;
 		Vector3 refPosition = this.refObject.transform.position;
 		Vector3 previousPosition = this.transform.position;
 		Vector3 newPosition = previousPosition;
+		float reverseXFactor = this.reverseX ? -1 : 1;
+		float reverseYFactor = this.reverseY ? -1 : 1;
 		if (this.horizontalDependent) {
-			newPosition.x = refPosition.x + refPosition.x * progressiveHorizontalOffset;
-			newPosition.x += horizontalOffset;
+			newPosition.x = refPosition.x + refPosition.x * progressiveHorizontalOffset * reverseXFactor;
+			newPosition.x += horizontalOffset * reverseXFactor;
 		}
 		if (this.verticalDependent) {
-			newPosition.y = refPosition.y + refPosition.x * progressiveVerticalOffset;
-			newPosition.y += verticalOffset;
+			newPosition.y = refPosition.y + refPosition.x * progressiveVerticalOffset * reverseYFactor;
+			newPosition.y += verticalOffset * reverseYFactor;
 		}
 
-		this.transform.position = Vector3.SmoothDamp(previousPosition, newPosition, ref this.velocity, smoothing);
+		if (smooth) {
+			transform.Translate(newPosition - previousPosition);
+		} else {
+			transform.position = newPosition;
+		}
 	}
 }
