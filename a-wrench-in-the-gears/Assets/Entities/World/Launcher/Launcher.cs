5020fd6bc39c6df3bdf3559928096fd453c7def4
SBats <simon.bats12@gmail.com>
2018-07-15 23:47:21 -0400

    Create Launcher and projectile scripts

---
+++ a-wrench-in-the-gears/Assets/Entities/World/Launcher/Launcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour {
+	public GameObject projectileType;
+	public float timeBetweenLaunch = 1f;
+	public Vector3 angle;
+
+	private float nextLaunch = 0f;
+
+	private void Start() {
+	}
+
+	private void Update() {
+		if (Time.time >= this.nextLaunch) {
+			this.LaunchProjectile();
+		}
+	}
+
+	private void LaunchProjectile() {
+		GameObject projectile = ObjectPooler.SharedInstance.GetPooledObject(this.projectileType.tag);
+		if (projectile) {
+			projectile.transform.position = transform.position;
+			projectile.transform.rotation = Quaternion.Euler(this.angle);
+			projectile.SetActive(true);
+			this.nextLaunch = Time.time + this.timeBetweenLaunch;
+		}
+	}
+}
---
+++ a-wrench-in-the-gears/Assets/Entities/World/Launcher/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	public float speed = 1f;
+	public List<GameObject> stoppers;
+
+	private void OnDisable() {
+		gameObject.SetActive(false);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log(other.gameObject.tag);
+		foreach (GameObject stopper in stoppers) {
+			Debug.Log(stopper.tag);
+			if (other.gameObject.tag == stopper.tag) {
+				gameObject.SetActive(false);
+			}
+		}
+	}
+
+	private void Update() {
+		transform.Translate(Vector3.up * speed * Time.deltaTime);
+	}
+}
