02ac354c6e38a285cb96bf42cd840118988483a3
SBats <simon.bats12@gmail.com>
2018-07-23 22:03:42 -0400

    Create a Coroutine to move elements

---
+++ a-wrench-in-the-gears/Assets/Scripts/CoroutineMover.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineMover {
+	public bool moving = false;
+
+	private AnimationCurve _animationCurve;
+	private GameObject _target;
+
+	public CoroutineMover(AnimationCurve animationCurve, GameObject target) {
+		this._animationCurve = animationCurve;
+		this._target = target;
+	}
+
+	public IEnumerator Move(Vector3 start, Vector3 end, float duration) {
+		float elapsedTime = 0f;
+
+		this.moving = true;
+		while (elapsedTime <= duration) {
+			elapsedTime += Time.deltaTime;
+
+			float animationProgression = Mathf.Clamp01(elapsedTime / duration);
+			float curvedProgression = this._animationCurve.Evaluate(animationProgression);
+			this._target.transform.position = Vector3.Lerp(start, end, curvedProgression);
+
+			yield return null;
+		}
+		this.moving = false;
+	}
+}
