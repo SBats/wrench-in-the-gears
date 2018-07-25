c83275a9b9a4c0b9765ac7f301a7fa0dd58b136e
SBats <simon.bats12@gmail.com>
2018-07-24 20:55:03 -0400

    Create base for boss cycle

--- a-wrench-in-the-gears/Assets/Entities/Characters/Bosses/Golem/GolemBossController.cs
+++ a-wrench-in-the-gears/Assets/Entities/Characters/Bosses/Golem/GolemBossController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum GolemBossStates {
-	Iddle,
+	Idle,
 	PunchLeft,
 	PunchRight,
 	SmashLeft,
@@ -14,25 +14,68 @@ public class GolemBossController : MonoBehaviour {
 	public GameObject head;
 	public GameObject leftHand;
 	public GameObject rightHand;
+	public float idleCicleDuration;
 
-	private GolemBossHeadController headController;
-	private GolemBossHandController leftHandController;
-	private GolemBossHandController rightHandController;
-	private GolemBossStates state;
+	private bool _isAlive = true;
+	private GolemBossHeadController _headController;
+	private GolemBossHandController _leftHandController;
+	private GolemBossHandController _rightHandController;
+	private GolemBossStates _state = GolemBossStates.Idle;
+	private GolemBossStates _previousState = GolemBossStates.Idle;
+	private bool _idleCoroutineIsRunning = false;
+
 
 	private void Awake() {
-		this.headController = this.head.GetComponent<GolemBossHeadController>();
-		this.leftHandController = this.leftHand.GetComponent<GolemBossHandController>();
-		this.rightHandController = this.rightHand.GetComponent<GolemBossHandController>();
+		this._headController = this.head.GetComponent<GolemBossHeadController>();
+		this._leftHandController = this.leftHand.GetComponent<GolemBossHandController>();
+		this._rightHandController = this.rightHand.GetComponent<GolemBossHandController>();
 	}
 
 	private void FixedUpdate() {
-		if (!this.leftHandController.isMoving()) {
-			this.Attack();
+		switch (this._state) {
+			case GolemBossStates.Idle:
+				if (!this._idleCoroutineIsRunning) {
+					StartCoroutine(this._Idle());
+				}
+				break;
+			case GolemBossStates.PunchLeft:
+				if (!this._leftHandController.isMoving()) {
+					StartCoroutine(this._PunchLeft());
+				}
+				break;
+			case GolemBossStates.PunchRight:
+				if (!this._rightHandController.isMoving()) {
+					StartCoroutine(this._PunchRight());
+				}
+				break;
 		}
 	}
 
-	private void Attack() {
-		this.leftHandController.Punch(this.leftHand.transform.position + Vector3.right * 6);
+	private IEnumerator _Idle() {
+		this._idleCoroutineIsRunning = true;
+		yield return new WaitForSeconds(this.idleCicleDuration);
+		switch (this._previousState){
+			case GolemBossStates.Idle:
+			case GolemBossStates.PunchRight:
+				this._state = GolemBossStates.PunchLeft;
+				break;
+			case GolemBossStates.PunchLeft:
+				this._state = GolemBossStates.PunchRight;
+				break;
+		}
+		this._idleCoroutineIsRunning = false;
+		this._previousState = GolemBossStates.Idle;
+	}
+
+	private IEnumerator _PunchLeft() {
+		yield return this._leftHandController.Punch(this.leftHand.transform.position + Vector3.right * 6);
+		this._previousState = GolemBossStates.PunchLeft;
+		this._state = GolemBossStates.Idle;
+	}
+
+	private IEnumerator _PunchRight() {
+		yield return this._rightHandController.Punch(this.rightHand.transform.position + Vector3.left * 6);
+		this._previousState = GolemBossStates.PunchRight;
+		this._state = GolemBossStates.Idle;
 	}
 }
--- a-wrench-in-the-gears/Assets/Entities/Characters/Bosses/Golem/GolemBossHandController.cs
+++ a-wrench-in-the-gears/Assets/Entities/Characters/Bosses/Golem/GolemBossHandController.cs
@@ -5,19 +5,59 @@ using UnityEngine;
 
 public class GolemBossHandController : MonoBehaviour {
 	public float punchSpeed;
+	public float punchPause;
 	public AnimationCurve animationCurve;
+	public GameObject hitboxContainer;
 
 	private CoroutineMover _mover;
+	private BoxCollider2D _hitbox;
+	private bool _moving;
+
+	//
+	// Lifecycle
+	//
 
 	private void Awake() {
 		this._mover = new CoroutineMover(animationCurve, this.gameObject);
+		this._hitbox = this.hitboxContainer.GetComponent<BoxCollider2D>();
 	}
 
-	public void Punch(Vector3 target) {
-		StartCoroutine(this._mover.Move(this.transform.position, target, this.punchSpeed));
+	private void OnEnable() {
+		this._disableHitbox();
+	}
+
+	//
+	// Public
+	//
+
+	public Coroutine Punch(Vector3 target) {
+		return StartCoroutine(this._PunchCoroutine(target));
 	}
 
 	public bool isMoving() {
-		return this._mover.moving;
+		return this._moving || this._mover.moving;
+	}
+
+	//
+	// Private
+	//
+
+	private void _enableHitbox() {
+		this._hitbox.enabled = true;
+	}
+
+	private void _disableHitbox() {
+		this._hitbox.enabled = false;
+	}
+
+	private IEnumerator _PunchCoroutine(Vector3 target) {
+		this._moving = true;
+		this._enableHitbox();
+		Vector3 currentPosition = this.transform.position;
+		yield return StartCoroutine(this._mover.Move(currentPosition, target, this.punchSpeed));
+		yield return new WaitForSeconds(this.punchPause);
+		this._disableHitbox();
+		yield return StartCoroutine(this._mover.Move(target, currentPosition, this.punchSpeed));
+		this._moving = false;
 	}
 }
