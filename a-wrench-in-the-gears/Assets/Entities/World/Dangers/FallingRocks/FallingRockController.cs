752c642fe41ffd7d404c676c4f24451af13e684a
SBats <simon.bats12@gmail.com>
2018-07-24 20:54:46 -0400

    Remove comments

--- a-wrench-in-the-gears/Assets/Entities/World/Dangers/FallingRocks/FallingRockController.cs
+++ a-wrench-in-the-gears/Assets/Entities/World/Dangers/FallingRocks/FallingRockController.cs
@@ -23,7 +23,6 @@ public class FallingRockController : MonoBehaviour {
 	private TriggerController triggerController;
 	private Vector3 currentTarget;
 	public bool triggerStatus;
-	// private bool moving = false;
 	private CoroutineMover mover;
 
 	private void Awake() {
@@ -80,26 +79,9 @@ public class FallingRockController : MonoBehaviour {
 	public void StartMovement() {
 		this.ToggleHitBox(this.currentTarget == this.bottom);
 		StartCoroutine(this.mover.Move(this.rock.transform.position, this.currentTarget, fallDuration));
-		// StartCoroutine(this.AnimatePosition(this.rock.transform.position, this.currentTarget, fallDuration));
 	}
 
 	private void ToggleHitBox(bool status) {
 		this.hitBoxCollider.enabled = status;
 	}
-
-	// IEnumerator AnimatePosition(Vector3 origin, Vector3 target, float duration) {
-	// 	float elapsedTime = 0f;
-
-	// 	this.moving = true;
-	// 	while (elapsedTime <= duration) {
-	// 		elapsedTime += Time.deltaTime;
-
-	// 		float animationProgression = Mathf.Clamp01(elapsedTime / duration);
-	// 		float curvedProgression = this.animationCurve.Evaluate(animationProgression);
-	// 		this.rock.transform.position = Vector3.Lerp(origin, target, curvedProgression);
-
-	// 		yield return null;
-	// 	}
-	// 	this.moving = false;
-	// }
 }
